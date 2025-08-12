9eef5abbc845b5fcac49e974ea916540aa5a40f0
Idzanami <Iliyaexyma@yandex.ru>
2025-08-12 16:08:01 +0300

    All is ready now

--- Pentagramm/Program.cs
+++ Pentagramm/Program.cs
@@ -87,11 +87,8 @@ namespace Pentagramm
             var app = builder.Build();
 
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
